38531955d22119d65faabf2af631d461d12550b3
bezgodoy <belisa.godoy@gmail.com>
2018-03-18 17:31:28 -0300

    adjusts landing 18.03 part I

--- template/js/custom.js
+++ template/js/custom.js
@@ -8,38 +8,3 @@
  * File Description: Place here your custom scripts
  */
 
-$('#navbar-collapse-1 > ul > li > a').on('click', function(e) {
-    var currentPos = $(window).scrollTop();
-    var target = $(e.currentTarget).attr('href');
-    var space =  Math.abs(currentPos - $(target).offset().top);
-
-    var speed = 0.6; // low speed for short distances
-    if(space > $('#about-section').outerHeight() + 5) {
-      speed = 1.5; // high speed for long distances
-    }
-
-    $('html, body').animate({
-        scrollTop: $(target).offset().top
-    }, space / speed);
-});
-
-$(window).on('scroll', function() {
-    var currentPos = $(window).scrollTop();
-
-    $('.section-btn').each(function() {
-        var sectionLink = $(this);
-
-        // capture the height of the navbar
-        var navHeight = $('.header').outerHeight() + 1;
-        var section = $(sectionLink.attr('href'));
-
-        // subtract the navbar height from the top of the section
-        if(section.position().top - navHeight  <= currentPos && sectionLink.offset().top + section.height() > currentPos) {
-            $('.navbar-right li').removeClass('active-btn');
-            sectionLink.parent().addClass('active-btn');
-        } else {
-            sectionLink.parent().removeClass('active-btn');
-        }
-    });
-});
-
